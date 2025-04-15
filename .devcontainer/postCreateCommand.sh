539b7bd1daf048998cb3502f3a771eba1462de49
Anton J <antonjavelosa@gmail.com>
2025-04-15 15:14:47 +0800

    Modified devcontainer to use zsh as default shell

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Install zsh and set it as default shell
+apt-get update && apt-get install -y zsh
+chsh -s $(which zsh)
+
 # Install tools specified in mise.toml
 #
-cd /workspaces/real-time-ml-system-cohort-4
+cd /workspaces/real-time-ml-system-4
 mise trust
 mise install
-echo 'eval "$(/usr/local/bin/mise activate bash)"' >> ~/.bashrc
-source ~/.bashrc
+
+# Configure zsh with mise
+echo 'eval "$(/usr/local/bin/mise activate zsh)"' >> ~/.zshrc
+
+# Start a new zsh session
+exec zsh
